146f23101036eb1024159903a556dbe4126eceae
gustavo@mumuki.org <gustavo@mumuki.org>
2021-12-17 18:46:50 +0000

    Mumuki Export on 2021-12-17 18:46:50 +0000

--- 00004_El derecho a la identidad/extra.py
+++
@@ -1,9 +0,0 @@
-class Superheroe:
-  def __init__(una_creacion):
-    self.creacion = una_creacion
-
-batman = Superheroe(1939)
-superman = Superheroe(1938)
-bruno_diaz = batman
-bruce_wayne = bruno_diaz
-clark_kent = superman
